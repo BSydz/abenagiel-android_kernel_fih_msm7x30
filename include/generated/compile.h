28b52ba161b06b45d10f6805a85b3aeea4b05e81
Joey Troy <joey@joeytroy.com>
2012-11-07 00:07:22 -0700

    first commit
    Source code created by abenagiel for fih boards 2.6.35.7

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Fri Nov 2 00:23:52 WIT 2012"
+#define LINUX_COMPILE_TIME "00:23:52"
+#define LINUX_COMPILE_BY "abenagiel"
+#define LINUX_COMPILE_HOST "abenagiel-mode-x"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
